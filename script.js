62e8406bdface8a0fd5c0fc62b68c0986a25a579
fxVaibhav <vaibhavsoni09122003@gmail.com>
2024-01-11 14:41:20 +0530

    files

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 function myFunction() {
   let c_e = document.createElement("textarea");
-  let add = c_e.setAttribute("class", "writing-area");
+  c_e.setAttribute("class", "writing-area");
+  c_e.setAttribute("cols", "30");
+  c_e.setAttribute("rows", "10");
+  c_e.setAttribute("placeholder", "Click Here");
+  document.body.appendChild(c_e);
 }
