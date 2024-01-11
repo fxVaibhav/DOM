1a1f229d689b38f0ae23546c71310c7208a379d8
fxVaibhav <vaibhavsoni09122003@gmail.com>
2024-01-11 16:37:44 +0530

    contact page but no responsive

--- script.js
+++
@@ -1,8 +0,0 @@
-function myFunction() {
-  let c_e = document.createElement("textarea");
-  c_e.setAttribute("class", "writing-area");
-  c_e.setAttribute("cols", "30");
-  c_e.setAttribute("rows", "10");
-  c_e.setAttribute("placeholder", "Click Here");
-  document.body.appendChild(c_e);
-}
